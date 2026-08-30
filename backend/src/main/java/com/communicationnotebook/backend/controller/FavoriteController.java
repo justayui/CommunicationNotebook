f54bb409505fc440016c00bcfd36b410732af39a
justayui <rina.s2.yui@gmail.com>
2026-08-30 19:14:31 +0900

    feat: FavoriteのuserId受け渡しをセッション認証ベースに変更

--- backend/src/main/java/com/communicationnotebook/backend/controller/FavoriteController.java
+++ backend/src/main/java/com/communicationnotebook/backend/controller/FavoriteController.java
@@ -1,12 +1,13 @@
 package com.communicationnotebook.backend.controller;
 
+import com.communicationnotebook.backend.security.UserPrincipal;
 import com.communicationnotebook.backend.service.FavoriteService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,13 +23,13 @@ public class FavoriteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void register(@PathVariable Integer noteId, @RequestParam Integer userId) {
-        favoriteService.register(noteId, userId);
+    public void register(@PathVariable Integer noteId, @AuthenticationPrincipal UserPrincipal principal) {
+        favoriteService.register(noteId, principal.getId());
     }
 
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void unregister(@PathVariable Integer noteId, @RequestParam Integer userId) {
-        favoriteService.unregister(noteId, userId);
+    public void unregister(@PathVariable Integer noteId, @AuthenticationPrincipal UserPrincipal principal) {
+        favoriteService.unregister(noteId, principal.getId());
     }
 }
