a7424afc3c90ba2df44d96d2377ed796aca6f04b
justayui <rina.s2.yui@gmail.com>
2026-09-08 22:27:42 +0900

    Applicationクラスへの＠OpenAPIDefinition付与

--- backend/src/main/java/com/communicationnotebook/backend/BackendApplication.java
+++ backend/src/main/java/com/communicationnotebook/backend/BackendApplication.java
@@ -1,8 +1,11 @@
 package com.communicationnotebook.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "連絡ノートアプリ", version = "1.0.0"))
 @SpringBootApplication
 public class BackendApplication {
 
