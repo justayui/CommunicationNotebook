9660111be0a9e1fd2b96d7e1769fcc4f3fcc79f9
justayui <rina.s2.yui@gmail.com>
2026-09-10 15:45:39 +0900

    CommentResponseクラスへのアノテーション追加

--- backend/src/main/java/com/communicationnotebook/backend/dto/CommentResponse.java
+++ backend/src/main/java/com/communicationnotebook/backend/dto/CommentResponse.java
@@ -1,10 +1,24 @@
 package com.communicationnotebook.backend.dto;
 
+
 import com.communicationnotebook.backend.entity.Comment;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 
+@Schema(description = "コメント情報を取得します")
 public record CommentResponse(
-        Integer id, Integer noteId, Integer userId, String author, String content, LocalDateTime createdAt) {
+        @Schema(description = "コメントのIDです。", example = "1")
+        Integer id, 
+        @Schema(description = "コメントに紐づく投稿のIDです。", example = "1")
+        Integer noteId, 
+        @Schema(description = "コメントを記載したユーザーのIDです。", example = "1")
+        Integer userId, 
+        @Schema(description = "コメントを記載したユーザーの名前です。", example = "田中")
+        String author, 
+        @Schema(description = "コメントの内容です。", example = "この投稿は非常に参考になりました。")
+        String content, 
+        @Schema(description = "コメント投稿時間です。LocalDateTimeが自動で設定されます。", example = "2026-04-01T16:00:00")
+        LocalDateTime createdAt) {
 
     public static CommentResponse from(Comment comment) {
         return new CommentResponse(
