bae1a04f5bf7bf09c4d9ada194725de8c0e4b282
justayui <rina.s2.yui@gmail.com>
2026-09-12 21:04:19 +0900

    NoteReaderResponseDTOへのアノテーション追加。

--- backend/src/main/java/com/communicationnotebook/backend/dto/NoteReaderResponse.java
+++ backend/src/main/java/com/communicationnotebook/backend/dto/NoteReaderResponse.java
@@ -1,8 +1,14 @@
 package com.communicationnotebook.backend.dto;
 
 import com.communicationnotebook.backend.entity.NoteRead;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record NoteReaderResponse(Integer userId, String name) {
+@Schema(description = "既読者の情報を表すDTOです。")
+public record NoteReaderResponse(
+    @Schema(description = "ユーザーのIDです。")
+    Integer userId, 
+    @Schema(description = "既読者の名前です。")
+    String name) {
 
     public static NoteReaderResponse from(NoteRead noteRead) {
         return new NoteReaderResponse(noteRead.getUser().getId(), noteRead.getUser().getName());
