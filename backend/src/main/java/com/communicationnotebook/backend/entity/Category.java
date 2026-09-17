fd1b0711eec3f90c9e31d3ed8464ba781a58beb6
justayui <rina.s2.yui@gmail.com>
2026-09-17 22:34:13 +0900

    カテゴリ関連のクラスへのJavaDoc追記

--- backend/src/main/java/com/communicationnotebook/backend/entity/Category.java
+++ backend/src/main/java/com/communicationnotebook/backend/entity/Category.java
@@ -9,16 +9,24 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * カテゴリ情報を表すエンティティです。categoriesテーブルと対応します。
+ */
 @Entity
 @Table(name = "categories")
 @Getter
 @Setter
 public class Category {
-
+    /**
+     * カテゴリのIDです。自動採番されます。
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "name", nullable = false)
+    /**
+     * カテゴリ名です。未入力不可です。一意である必要があります。
+     */
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
 }
--- backend/src/main/java/com/communicationnotebook/backend/repository/CategoryRepository.java
+++ backend/src/main/java/com/communicationnotebook/backend/repository/CategoryRepository.java
@@ -4,7 +4,15 @@ import com.communicationnotebook.backend.entity.Category;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * categoriesテーブルと紐づくリポジトリです。
+ */
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
+    /**
+     * カテゴリを全件取得しidカラムで昇順に並べ替え返却します。
+     *
+     * @return カテゴリ情報（全件）
+     */
     List<Category> findAllByOrderByIdAsc();
 }
