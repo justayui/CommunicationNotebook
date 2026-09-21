8475ac5db0ebb2cbab45edf26c9ea97d79a1206f
justayui <rina.s2.yui@gmail.com>
2026-09-21 15:16:54 +0900

    NoteReadクラスへのJavaDoc追記

--- backend/src/main/java/com/communicationnotebook/backend/entity/NoteRead.java
+++ backend/src/main/java/com/communicationnotebook/backend/entity/NoteRead.java
@@ -13,24 +13,38 @@ import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 投稿の既読情報を表すエンティティです。note_readsテーブルと対応します。
+ */
 @Entity
 @Table(name = "note_reads")
 @Getter
 @Setter
 public class NoteRead {
-
+    /**
+     * 主キーとなるIDです。自動採番されます。
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    /**
+     * ユーザー情報です。既読とユーザーは多対一の関係です。未入力不可です。
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    /**
+     * 投稿情報です。既読と投稿は多対一の関係です。未入力不可です。
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "note_id", nullable = false)
     private Note note;
 
+    /**
+     * 既読登録した日時です。LocalDateTimeが自動で設定されます。
+     */
     @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
     private LocalDateTime createdAt;
 }
